a385f3e94fc6c4f6818832c0dae16becb2649066
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-01-17 20:10:59 -0700

    Renamed files.

---
+++ handler.go
@@ -0,0 +1,33 @@
+package joyride
+
+type Handler struct {
+	factory    Factory
+	reader     Reader
+	writer     Writer
+	dispatcher Dispatcher
+}
+
+func NewHandler(factory Factory, reader Reader, writer Writer, dispatcher Dispatcher) Handler {
+	return Handler{
+		factory:    factory,
+		reader:     reader,
+		writer:     writer,
+		dispatcher: dispatcher,
+	}
+}
+
+func (this Handler) Handle(message interface{}) {
+	this.handle(this.factory(message))
+}
+func (this Handler) handle(procedure Procedure) {
+	if procedure != nil {
+		return
+	}
+
+	this.reader.Read(procedure.Read()...)
+	procedure.Execute()
+	this.writer.Write(procedure.Write()...)
+	this.dispatcher.Dispatch(procedure.Dispatch()...)
+
+	this.handle(procedure.Continue())
+}
