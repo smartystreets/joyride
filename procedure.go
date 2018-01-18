bede99a8100df667e797dd3f7a964b81d29246db
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-01-17 21:47:15 -0700

    Convenience methods.

--- procedure.go
+++ procedure.go
@@ -15,3 +15,20 @@ func (this *BaseProcedure) Execute()                { /* noop */ }
 func (this *BaseProcedure) Write() []interface{}    { return this.Writes }
 func (this *BaseProcedure) Dispatch() []interface{} { return this.Messages }
 func (this *BaseProcedure) Continue() Procedure     { return this.Next }
+
+func (this *BaseProcedure) AppendReads(reads ...interface{}) *BaseProcedure {
+	this.Reads = append(this.Reads, reads...)
+	return this
+}
+func (this *BaseProcedure) AppendWrites(writes ...interface{}) *BaseProcedure {
+	this.Reads = append(this.Writes, writes...)
+	return this
+}
+func (this *BaseProcedure) AppendMessages(messages ...interface{}) *BaseProcedure {
+	this.Messages = append(this.Messages, messages...)
+	return this
+}
+func (this *BaseProcedure) AppendNext(next Procedure) *BaseProcedure {
+	this.Next = next
+	return this
+}
