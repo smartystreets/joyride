eb94a54d966568f126b9065ccf54e8837997535e
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-01-17 21:53:03 -0700

    Functional options.

--- procedure.go
+++ procedure.go
@@ -7,28 +7,24 @@ type BaseProcedure struct {
 	Next     Procedure
 }
 
-func NewProcedure(reads ...interface{}) *BaseProcedure {
-	return &BaseProcedure{Reads: reads}
+type Option func(*BaseProcedure)
+
+func WithReads(reads ...interface{}) Option {
+	return func(this *BaseProcedure) { this.Reads = append(this.Reads, reads...) }
 }
+func WithWrites(writes ...interface{}) Option {
+	return func(this *BaseProcedure) { this.Writes = append(this.Writes, writes...) }
+}
+func WithMessages(messages ...interface{}) Option {
+	return func(this *BaseProcedure) { this.Messages = append(this.Messages, messages...) }
+}
+
+func NewProcedure(options ...Option) *BaseProcedure {
+	return nil
+}
+
 func (this *BaseProcedure) Read() []interface{}     { return this.Reads }
 func (this *BaseProcedure) Execute()                { /* noop */ }
 func (this *BaseProcedure) Write() []interface{}    { return this.Writes }
 func (this *BaseProcedure) Dispatch() []interface{} { return this.Messages }
 func (this *BaseProcedure) Continue() Procedure     { return this.Next }
-
-func (this *BaseProcedure) AppendReads(reads ...interface{}) *BaseProcedure {
-	this.Reads = append(this.Reads, reads...)
-	return this
-}
-func (this *BaseProcedure) AppendWrites(writes ...interface{}) *BaseProcedure {
-	this.Reads = append(this.Writes, writes...)
-	return this
-}
-func (this *BaseProcedure) AppendMessages(messages ...interface{}) *BaseProcedure {
-	this.Messages = append(this.Messages, messages...)
-	return this
-}
-func (this *BaseProcedure) AppendNext(next Procedure) *BaseProcedure {
-	this.Next = next
-	return this
-}
