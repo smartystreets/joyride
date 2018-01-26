b00e1f18065591f1a9cf972d502930c6bccc8671
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-01-26 11:21:06 -0700

    Experimenting with idea of composite task and base handler.

---
+++ composite_task.go
@@ -0,0 +1,54 @@
+package joyride
+
+type CompositeTask struct {
+	tasks []RunnableTask
+}
+
+func NewCompositeTask(tasks ...RunnableTask) CompositeTask {
+	var filtered []RunnableTask
+
+	for _, task := range tasks {
+		if task != nil {
+			filtered = append(filtered, task)
+		}
+	}
+
+	return CompositeTask{tasks: filtered}
+}
+
+func (this CompositeTask) Reads() (reads []interface{}) {
+	for _, task := range this.tasks {
+		reads = append(reads, task.Reads()...)
+	}
+	return reads
+}
+
+func (this CompositeTask) Execute() {
+	for _, task := range this.tasks {
+		task.Execute()
+	}
+}
+
+func (this CompositeTask) Writes() (writes []interface{}) {
+	for _, task := range this.tasks {
+		writes = append(writes, task.Writes()...)
+	}
+	return writes
+}
+
+func (this CompositeTask) Messages() (messages []interface{}) {
+	for _, task := range this.tasks {
+		messages = append(messages, task.Messages()...)
+	}
+	return messages
+
+}
+
+func (this CompositeTask) Next() RunnableTask {
+	var tasks []RunnableTask
+	for _, task := range this.tasks {
+		tasks = append(tasks, task.Next())
+	}
+
+	return NewCompositeTask(tasks...)
+}
---
+++ handler.go
@@ -0,0 +1,45 @@
+package joyride
+
+import (
+	"log"
+	"reflect"
+)
+
+type MessageHandler interface {
+	Handle(...interface{})
+	Apply(interface{}) bool
+	Run()
+}
+
+type Handler struct {
+	runner TaskRunner
+	inner  MessageHandler
+	tasks  []RunnableTask
+}
+
+func NewHandler(inner MessageHandler, runner TaskRunner, tasks ...RunnableTask) *Handler {
+	return &Handler{inner: inner, runner: runner, tasks: tasks}
+}
+
+func (this *Handler) Handle(messages ...interface{}) {
+	for _, message := range messages {
+		if !this.inner.Apply(message) {
+			log.Panicf("[WARN] Handler of type [%s] unable to handle message of type [%s].",
+				reflect.TypeOf(this.inner), reflect.TypeOf(message))
+		}
+	}
+
+	this.inner.Run()
+}
+
+func (this *Handler) Apply(_ interface{}) bool {
+	return false
+}
+
+func (this *Handler) Add(task RunnableTask) {
+	this.tasks = append(this.tasks, task)
+}
+
+func (this *Handler) Run() {
+	this.runner.Run(NewCompositeTask(this.tasks...))
+}
