5c62e68b9b3ba420c57481aaf1d4693f852b85e1
Michael Whatcott <mdwhatcott@gmail.com>
2020-02-18 16:56:06 -0700

    Simplified tasks.

--- examples/todo/tasks.go
+++ examples/todo/tasks.go
@@ -4,22 +4,22 @@ import "github.com/smartystreets/joyride/v2"
 
 type ListTODOsTask struct {
 	*joyride.Task
-	storage *SelectTODOs
+	query   *SelectTODOs
 	context *ListTODOs
 }
 
 func NewListTODOsTask(context *ListTODOs) *ListTODOsTask {
 	this := &ListTODOsTask{
 		Task:    joyride.NewTask(),
-		storage: &SelectTODOs{},
+		query:   &SelectTODOs{},
 		context: context,
 	}
-	this.PrepareRead(this.storage)
+	this.PrepareRead(this.query)
 	return this
 }
 
 func (this *ListTODOsTask) Run() {
-	for _, record := range this.storage.Results {
+	for _, record := range this.query.Results {
 		this.context.Results = append(this.context.Results, TODO{
 			Description: record.Description,
 			Completed:   record.Completed,
@@ -31,34 +31,22 @@ func (this *ListTODOsTask) Run() {
 
 type AddTODOTask struct {
 	*joyride.Task
-	description string
 }
 
 func NewAddTODOTask(context AddTODO) *AddTODOTask {
 	return &AddTODOTask{
-		Task:        joyride.NewTask(),
-		description: context.Description,
+		Task: joyride.NewTask(joyride.WithPreparedWrite(InsertTODO{Description: context.Description})),
 	}
-}
-
-func (this *AddTODOTask) Run() {
-	this.PrepareWrite(InsertTODO{Description: this.description})
 }
 
 //////////////////////////////////////////////////////////////////////////
 
 type CompleteTODOTask struct {
 	*joyride.Task
-	description string
 }
 
 func NewCompleteTODOTask(context CompleteTODO) *CompleteTODOTask {
 	return &CompleteTODOTask{
-		Task:        joyride.NewTask(),
-		description: context.Description,
+		Task: joyride.NewTask(joyride.WithPreparedWrite(UpdateTODO{Description: context.Description})),
 	}
-}
-
-func (this *CompleteTODOTask) Run() {
-	this.PrepareWrite(UpdateTODO{Description: this.description})
 }
