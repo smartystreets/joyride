0cef26797a45f1c6b3e1eab07907f37263fb8a77
Michael Whatcott <mdwhatcott@gmail.com>
2020-02-27 17:01:30 -0700

    DONE

--- handler.go
+++ handler.go
@@ -36,7 +36,7 @@ func (this *Handler) Add(task Executable) {
 }
 
 func (this *Handler) Run() {
-	this.runner.Run(CompositeTask(this.tasks)) // TODO: Clear tasks when Run is complete (bug)
+	this.runner.Run(CompositeTask(this.tasks))
 	this.tasks = nil
 }
 
